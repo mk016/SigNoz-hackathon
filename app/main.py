fb124e8c07ce6ace1e91afc37d170a001d6b4be5
mk016 <mahendrakumawat80224@gmail.com>
2026-07-24 15:44:00 +0530

    fix: update CORS middleware to disable credentials with wildcard origins

--- app/main.py
+++ app/main.py
@@ -65,7 +65,7 @@ instrument_app(app)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
